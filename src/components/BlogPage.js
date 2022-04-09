78370e8657b7d471ac44c0373764a19df65296a5
zio7711 <zi.ao.tan@outlook.com>
2022-04-09 18:00:29 +0800

    add blog page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,6 +1,10 @@
+import AnchorComponent from "./subComponents/Anchor";
+import BlogComponent from "./BlogComponent";
+import { Blogs } from "../data/BlogData";
 import LogoComponent from "./subComponents/LogoComponent";
 import PowerButton from "./subComponents/PowerButton";
 import React from "react";
+import SocialIcons from "./subComponents/SocialIcons";
 import img from "../assets/Images/patrick-tomasso.jpg";
 import styled from "styled-components";
 
@@ -10,9 +14,6 @@ const MainContainer = styled.div`
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-position: center;
-
-  width: 100vw;
-  height: 100vh;
 `;
 
 const Container = styled.div`
@@ -22,12 +23,35 @@ const Container = styled.div`
   position: relative;
   padding-bottom: 5rem;
 `;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
 const BlogPage = () => {
   return (
     <MainContainer>
       <Container>
         <LogoComponent />
         <PowerButton />
+        <SocialIcons />
+        <AnchorComponent />
+
+        <Center>
+          <Grid>
+            {Blogs.map((blog) => {
+              return <BlogComponent key={blog.id} blog={blog} />;
+            })}
+          </Grid>
+        </Center>
       </Container>
     </MainContainer>
   );
